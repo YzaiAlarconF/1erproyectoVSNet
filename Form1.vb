79f43b09b9dfa625fdea0350a6b53a432c573770
Yzai Alarcon Franca <alarconfrancay@gmail.com>
2021-10-26 15:10:56 -0500

    Primer proyecto

---
+++ Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+
+#   seleccionando el boton para al darle clic cambie el texto
+    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+        TextBox1.Text = "Texto Cambiado"
+    End Sub
+End Class
